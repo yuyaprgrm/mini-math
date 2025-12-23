f0a6d2f1d42a0ef0f991baf308ac33c6b1dad466
yuyaprgrm <yuyaprgrm@outlook.jp>
2025-12-23 20:56:36 +0900

    change new into try_new based on TDD

--- src/vector.rs
+++ src/vector.rs
@@ -1,4 +1,6 @@
 pub enum VectorError {
+    InvalidDimension,
+    OutOfBounds,
 }
 
 pub type Result<T> = std::result::Result<T, VectorError>;
@@ -8,11 +10,35 @@ pub struct Vector {
 }
 
 impl Vector {
-    pub fn new(data: Vec<f32>) -> Vector {
-        Self { data }
+    pub fn try_new(data: Vec<f32>) -> Result<Vector> {
+        if data.len() == 0 {
+            return Err(VectorError::InvalidDimension);
+        }
+
+        if data.iter().any(|x| x.is_nan() || x.is_infinite()) {
+            return Err(VectorError::OutOfBounds)
+        }
+
+        Ok(Vector { data })
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::vector::Vector;
+
+    #[test]
+    fn test_try_new_success_on_finite() {
+        assert!(Vector::try_new(vec![0.0, 1.0, f32::MAX]).is_ok())
+    }
+
+    #[test]
+    fn test_try_new_failure_on_infinite() {
+        assert!(Vector::try_new(vec![1.0, 2.0, f32::INFINITY]).is_err());
+    }
+
+    #[test]
+    fn test_try_new_failure_on_zero_dimension() {
+        assert!(Vector::try_new(vec![]).is_err())
+    }
 }
